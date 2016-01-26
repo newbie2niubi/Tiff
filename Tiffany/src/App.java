9d85c8af6a3d5b73236f09764b728ad6b4d97f73
wenwei2000@gmail.com <wenwei2000@gmail.com>
2016-01-25 23:51:14 -0800

    changes made

--- Tiffany/src/App.java
+++ Tiffany/src/App.java
@@ -2,9 +2,7 @@
 public class App {
 	public static void main(String[] args) {
 		Person person1 = new Person("Tiffany", 26);
-		Person person2 = new Person("Wei",33);
 		System.out.println(person1.toString());
-		System.out.println(person2.toString());
 		
 	}
 }
