e4ed7b6e31959fcb6b5756a8b306bfd181257336
wenwei2000@gmail.com <wenwei2000@gmail.com>
2016-01-25 23:47:54 -0800

    Initial commit

---
+++ Tiffany/src/App.java
@@ -0,0 +1,10 @@
+
+public class App {
+	public static void main(String[] args) {
+		Person person1 = new Person("Tiffany", 26);
+		Person person2 = new Person("Wei",33);
+		System.out.println(person1.toString());
+		System.out.println(person2.toString());
+		
+	}
+}
